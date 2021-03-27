4ce16192dbf48a5fa85d75878ece3702eafeeff2
Jacqueline Rodney <jacquelinerodney7@gmail.com>
2021-03-27 18:00:43 -0400

    manager prompt

---
+++ index.js
@@ -0,0 +1,67 @@
+//Require modules const fs = require('fs');
+const inquirer = require('inquirer');
+const fs = require ('fs');
+const { type } = require('node:os');
+const Manager = require (".lib/manager");
+const Engineer = require (".lib/engineer");
+const Intern = require ("./lib/intern");
+
+
+const team = []
+
+
+
+//AS A manager
+//I WANT to generate a webpage that displays my team's basic info
+//SO THAT I have quick access to their emails and GitHub profiles
+
+
+//inquirer prompt to get user input for manager
+
+const ManagerInfo = () => {
+      inquirer .prompt ([ {
+            type:"input",
+            message:"What is the team manager's name?",
+            name: "name",
+       },
+       {
+            type:"input",
+            message:"What is the team manager's employee ID?",
+            name: "id",
+
+       },
+       {
+            type:"input",
+            message:"What is the team manager's email?",
+            name: "email",
+       },
+       {
+            type:"input",
+            message:"What is the team manager's office number?",
+            name: "officeNumber",
+       }
+      ])
+};
+      //Manager Constructor
+      
+            const {name, id, email, officeNumber} = mangerInfo
+            const Manager = new Manger(name, id, email, officeNumber)
+            team.push(manager);
+            nextOption()
+    
+
+
+//inqurier prompt for employee type input
+      
+const employeeType = () => {
+      inquirer .prompt ([  {
+            type:"list",
+            message:"What team member would you like to add next?",
+            choices: ["Engineer", "Intern"],
+            name: "employeeType"
+      }])
+
+//
+
+
+}
