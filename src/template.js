1d4a499e48b2a7ce3d1622a461b395553bf9a5ec
Jacqueline Rodney <jacquelinerodney7@gmail.com>
2021-04-05 00:49:00 -0400

    psuedo

---
+++ src/template.js
@@ -0,0 +1,33 @@
+const generateTeam = team => {
+
+    //manager 
+
+
+
+
+
+}
+
+const generateEngineer = engineer => {
+
+
+    //engineer
+
+
+}
+
+const generateIntern = intern => {
+
+
+    // intern
+
+
+}
+
+// push team info Manager . Engineer. Intern
+
+//export team
+
+
+return 
+//team.html file with ${generateTeam(team)}
