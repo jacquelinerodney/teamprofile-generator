109182426724627c08154b7dc75f22b99e7477f6
Jacqueline Rodney <jacquelinerodney7@gmail.com>
2021-04-06 18:19:18 -0400

    generate html

--- src/template.js
+++ src/template.js
@@ -82,10 +82,36 @@ module.exports = team => {}
 
 return `
 
-//team.html file with ${generateTeam(team)}
-
-
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>My Team</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        
+    </head>
+    <body>
+        //jumbotron "My Team" container
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-heading">
+                    <h1 class="text-center">My Team</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+        <div class="row">
+            <div class="team-area col-12 d-flex justify-content-center">
+                  ${generateTeam(team)}
+            </div>
+            </div>
+        </div>
+</body>
+<html>
+    ;
 `
 
 
