5d619e1b104f0485bb3b453384098df7d9ed07bb
Jacqueline Rodney <jacquelinerodney7@gmail.com>
2021-04-09 20:18:01 -0400

    debugging

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const inquirer = require('inquirer');
 const fs = require ('fs');
 const generateHTML = require("./src/template.js");
 
-const Manager = require (".lib/manager");
-const Engineer = require (".lib/engineer");
+const Manager = require ("./lib/manager");
+const Engineer = require ("./lib/engineer");
 const Intern = require ("./lib/intern");
 
 
